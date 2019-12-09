f4b479592390dc9fffd4ffb15f6d2984333e3dec
Phil Chen <06fahchen@gmail.com>
2019-12-09 12:21:18 +0800

    init rollup

---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+import { version } from './package.json';
+
+import resolve from 'rollup-plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
+
+const production = !process.env.ROLLUP_WATCH;
+
+export default {
+  input: 'src/main.js',
+  output: {
+    file: `dist/jet-app-utils-${version}.js`,
+    format: 'esm',
+    sourcemap: true,
+  },
+  plugins: [
+    resolve(),
+    production && terser(), // minify, but only in production
+  ]
+};
