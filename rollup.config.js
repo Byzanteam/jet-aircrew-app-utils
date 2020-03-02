0ab0db97e5daeeeaaef9322e512e5f0e8b12adb7
leecason <leecason0722@gmail.com>
2020-03-02 13:51:31 +0800

    ✨: build utilities_classes.css

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,39 @@
 import typescript from '@rollup/plugin-typescript';
 import resolve from 'rollup-plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
+import postcss from 'rollup-plugin-postcss';
+
 import { version } from './package.json';
 
 const production = !process.env.ROLLUP_WATCH;
 
-export default {
-  input: 'src/main.js',
-  output: {
-    file: `dist/jet-app-utils-${version}.js`,
-    format: 'esm',
-    sourcemap: true,
+export default [
+  {
+    input: 'src/lib/script/index.ts',
+    output: {
+      file: `dist/jet-app-utils-${version}.js`,
+      format: 'esm',
+      sourcemap: true,
+    },
+    plugins: [
+      resolve(),
+      production && terser(), // minify, but only in production
+      typescript(),
+    ],
   },
-  plugins: [
-    resolve(),
-    production && terser(), // minify, but only in production
-    typescript(),
-  ],
-};
+  {
+    input: 'src/lib/style/index.scss',
+    output: {
+      file: 'dist/utilities_classes.css',
+      format: 'cjs',
+    },
+    plugins: [
+      postcss({
+        extract: true,
+        plugins: [],
+        extensions: ['.css', '.scss', '.sass'],
+        minimize: true,
+      }),
+    ],
+  },
+];
